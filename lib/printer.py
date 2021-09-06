5598839b3318307a52e961cb2e30e03fac1a27ca
Planck <maxplanck817@gmail.com>
2021-09-06 21:28:10 +0530

    column

--- lib/printer.py
+++ lib/printer.py
@@ -1,13 +1,14 @@
 
-def printer(x):
-    m=x.shape[-1]
-    k=x.shape[0]
+def printer(x,column=True):
     s=30
+    k=x.shape[0]
+    m=x.shape[-1]
     print("-"*((s+1)*k+1))
     for i in range(m):
         for j in range(k):
             print("|{0:>{space}}".format(x[i][j],space=s),end="")
             if j==k-1:
                 print("|")
+                if column==True and i==0:
+                    print("-"*((s+1)*k+1))
     print("-"*((s+1)*k+1))
-    return(0)
