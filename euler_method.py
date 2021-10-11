218f27a128180a35ec66cd4959709e5a2c65d4b6
Planck <maxplanck817@gmail.com>
2021-10-11 11:23:10 +0530

    Euler_mathod: CLEAN UP

--- euler_method.py
+++ euler_method.py
@@ -1,61 +1,31 @@
 import numpy as np
 
-def euler_method(f,d=(0,5),i=(0,0),N=7):
+def ode_solver(f,d=(0,5),i=(0,0),N=7,method = "ef" ):
+    if method not in ["ef","em","rk2","rk4"]:
+        raise ValueError("Method not supported")
     a,b = d
     t0,x0 = i
-    h = (b - a)/(N+1)
     ii = np.linspace(a,b,N+2)
-    newlist = []
-    x = i[1]
-    for j in ii:
-        x+= j + f(j,x)*h
-        newlist.append(x)
-    return(x,newlist,ii)
-
-
-def midpoint_euler(f,d=(0,5),i=(0,0),N=7):
-    a,b = d
-    t0,x0 = i
-    h = (b - a)/(N+1)
-    ii = np.linspace(a,b,N+2)
-    newlist = []
-    xi = t0
-    for j in ii:
-        xi+= j + f(j+h/2,xi + (h/2)*(f(j,xi)))*h
-        newlist.append(xi)
-    return(xi,newlist,ii)
-
-
-def rk2(f,d=(0,5),i=(0,0),N=7):
-    a,b = d
-    t0,x0 = i
-    h = (b - a)/(N+1)
-    ii = np.linspace(a,b,N+2)
-    newlist = []
-    xi = t0
-    for j in ii:
-        avg_slope = (f(j,xi) + f(j+1,xi + h*f(j,xi)))/2
-        xi += j + avg_slope*h
-        newlist.append(xi)
-    return(xi,newlist,ii)
-
-
-def rk4(f,d=(0,5),i=(0,0),N=7):
-    a,b = d
-    t0,x0 = i
-    h = (b - a)/(N+1)
-    ii = np.linspace(a,b,N+2)
-    xi = a
-    newlist = []
-    for j in ii:
-        m1 = f(j,xi)
-        m2 = f(j+h/2,xi + h/2*m1)
-        m3 = f(j+h/2,xi + h/2*m2)
-        m4 = f(j+h,xi + h*m3)
-        avg_slope = ( m1 +2*(m2 +m3) + m4 )/2
-        xi += j + avg_slope*h
-        newlist.append(xi)
-    return(xi,newlist,ii)
+    h = np.diff(ii)[0]
+    x = [x0]
+    if method == "ef":
+        for j in ii[1:]:
+            x.append(j + f(j,x[-1])*h)
+    elif method == "em":
+        for j in ii[1:]:
+            x.append(j + f(j+h/2,x[-1] + (h/2)*(f(j,x[-1])))*h)
+    elif method == "rk2":
+        for j in ii[1:]:
+            x.append(j + (f(j,x[-1]) + f(j+1,x[-1] + h*f(j,x[-1])))/2*h)
+    elif method== "rk4" :
+        for j in ii[1:]:
+            m1 = f(j,x[-1])
+            m2 = f(j+h/2,x[-1] + h/2*m1)
+            m3 = f(j+h/2,x[-1] + h/2*m2)
+            m4 = f(j+h,x[-1] + h*m3)
+            avg_slope = ( m1 +2*(m2 +m3) + m4 )/6
+            x.append(j + avg_slope*h)
+    return(x[-1],x,ii)
 
 def pf(x):
     y = ["nodes","euler method(Forward)","euler method(Centered)", "rk2","rk4"]
@@ -64,24 +34,32 @@ def pf(x):
 
 def forall(f,dom,ini):
     N=100
-    return(np.array([np.linspace(dom[0],dom[1],N+2),euler_method(f,dom,ini,N=N)[1],midpoint_euler(fx,dom,ini,N=N)[1],rk2(fx,dom,ini,N=N)[1],rk4(fx,dom,ini,N=N)[1]],dtype=float))
+    return(np.array([np.linspace(dom[0],dom[1],N+2),ode_solver(f,dom,ini,N=N)[1],ode_solver(fx,dom,ini,N=N,method="em")[1],ode_solver(fx,dom,ini,N=N,method="rk2")[1],ode_solver(fx,dom,ini,N=N,method="rk4")[1]],dtype="double"))
     
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
+    
+    
+    radx = lambda t,N : -N/4
+    dom = (0,0.02)
+    ini = (0,2e+4)
+    print(len(ode_solver(radx,dom,ini,N=100)[1]))
+    ta2 = forall(radx,dom,ini)
+    pf(ta2)
+    plt.yscale("log")
+    
+    plt.show()
+
+
+
+
+
+    '''
     fx = lambda t,x : np.sin(x*t)
     
-    '''
     t,x = np.linspace(0,1,20),np.linspace(0,1,20)
     T,X = np.meshgrid(t,x)
     f_i = 1/np.sqrt(1+f(T,X)**2)
     f_j = f(t,x)/np.sqrt(1+f(T,X)**2)
     plt.quiver(T,X,f_i,f_j)'''
-    
-    radx = lambda t,N : -N/4
-    dom = (0,5)
-    ini = (0,2e+4)
-    ta2 = forall(radx,dom,ini)    
-    pf(ta2)
-    
-    plt.show()
 
