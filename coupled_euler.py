5cf525833ba32917ba5cebac146b1a5f97dd5729
Planck <maxplanck817@gmail.com>
2021-10-21 00:13:41 +0530

    corrected -_-

--- coupled_euler.py
+++ coupled_euler.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pandas as pd
 def put(x,i,v):
     np.put(x,i,v)
     return(x)
 
-def rk4(x,f,h,i):
+def rk4(y,f,h,i):
+    x = np.array(y)
     x_i,t = x[i],x[0] 
     m1 = f[i-1](*x)
     m2 = f[i-1](*put(x,[0,i],[t+h/2,x_i+h/2*m1]))
@@ -12,26 +14,27 @@ def rk4(x,f,h,i):
     avg_m = ( m1 + 2*(m2+m3) + m4 )/6
     return(x_i + h*avg_m)
 
-def rk2(i,x,f,h):
+def rk2(i,y,f,h):
+    x = np.array(y)
     return(x[i] + (f[i-1](*x) + f[i-1](*put(x,[0,i],[x[0]+h,x[i] + h*f[i-1](*x)])
     ))/2*h)
 
-def em(i,x,f,h):
+def em(i,y,f,h):
+    x = np.array(y)
     return(x[i] + f[i-1](*put(x,[0,i],[x[0]+h/2,x[i] + (h/2)*f[i-1](*x)]))*h)
 
-def ef(i,x,f,h):
+def ef(i,y,f,h):
+    x = np.array(y)
     return(x[i] + h* f[i-1](*x))
 
 def ode_solver(func,t_axis,ini,Nh,n,method=rk4):
     N,h = Nh
-    data = np.zeros((N+2,n))
+    data = np.zeros((N+2,n),dtype="double")
     data[:,0],data[0,:] = t_axis, np.array(ini)
     params =  np.arange(1,n)
-    iter = np.vectorize(method, excluded=["f","h","x"]) 
+    iter = np.vectorize(method, excluded=["f","h","y"]) 
     for j in range(0,N+1) :
-        data[j+1,1:] = iter(x=data[j],f=func,h=h,i=params)
-        print(data[j+1],j+1)
-    print(data)
+        data[j+1,1:] = iter(y=data[j],f=func,h=h,i=params)
     return(data)
 
 class set_problem:
@@ -75,11 +78,8 @@ if __name__ == "__main__" :
                      (0,0,-1),N =500)
     data_rk4,data_rk2,data_em,data_ef = ivp.rk4(),ivp.rk2(),ivp.ef(),ivp.em() 
     fig,(ax1,ax2) = plt.subplots(2,1)
-    fig2,ax3 = plt.subplots(1,1)
-    ivp.jt_plot(ax1,1)
-    ivp.jt_plot(ax2,2)
-    ivp.kj_plot(ax3,1,2)
-
-    ax1.legend(),ax2.legend(),ax3.legend()
+    fig2,a = plt.subplots(1,1)
+    ivp.kj_plot(a,1,2)
+    ax1.legend(),ax2.legend(),a.legend()
     plt.show()
 
--- coupled_oscillators.py
+++ coupled_oscillators.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from coupled_euler import *
 import pandas as pd
 import math
-l,c1,c2 = 1.3,1,1.3
-r=100
+l,c1,c2 = 0.1,1,1.3
+r=0
 ivp = set_problem(f=[lambda t,x,y,q1,q2,q3 : (q1/c1 - q2/c2 -r*x)/l,lambda t,x,y,q1,q2,q3 :(q2/c2 - q3/c1 -r*y)/l,
 lambda t,x,y,q1,q2,q3 : -x ,lambda t,x,y,q1,q2,q3 : -x-y,lambda t,x,y,q1,q2,q3 : y],
-dom=(0,1),
-ini=(0,-9999,-9999,-10,0,10),
-N=int(1e+1))
-ivp.rk4()
+dom=(0,15),
+ini=(0,10000,10000,1,0,-1),
+N=int(20000))
+d=ivp.rk4()
 fig,ax = plt.subplots(1,1)
 ivp.jt_plot(ax,1)
 ivp.jt_plot(ax,2)
